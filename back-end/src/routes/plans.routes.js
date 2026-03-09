cba2fa0af691df929c1a2f0d21644cf49c0839ad
Ahmed Gharib <ahmedtaher212005@gmail.com>
2026-03-09 19:46:05 +0200

    .

--- back-end/src/routes/plans.routes.js
+++ back-end/src/routes/plans.routes.js
@@ -1,11 +1,13 @@
 import express from "express";
 import { authenticateToken } from "../middleware/auth.js";
 import { getPlanById, listPlans } from "../controllers/lessonPlans.controller.js";
+import { exportPlanHandler } from "../controllers/export.controller.js";
 
 const router = express.Router();
 
 router.use(authenticateToken);
 router.get("/", listPlans);
+router.get("/:id/export", exportPlanHandler);
 router.get("/:id", getPlanById);
 
 export default router;
